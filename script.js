e923374d5ec809bda65fe904ff34d6a41b5bded0
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-02-16 21:07:57 +1300

    Finish validatePassword & submitForm refactor

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-function formListener () {
-    const h1 = document.querySelector("h1");
-    const h2 = document.querySelector("h2");
-    const form = document.querySelector("form");
-    form.addEventListener("submit", (e) => {
+const h1 = document.querySelector("h1");
+const h2 = document.querySelector("h2");
+const form = document.querySelector("form");
+
+function submitForm (e) {
+    validatePassword;
+
+    if (form.checkValidity()) {
         e.preventDefault();
         h2.textContent = "";
         h1.textContent = "Success - Have a Profitable Day.";
-    });
+    } else {
+        e.preventDefault();
+        form.reportValidity();
+    }
 }
 
-function doPasswordsMatch () {      //fix
-    const pw1 = document.querySelector("#pw1");
-    const pw2 = document.querySelector("#pw2");
-    pw2.addEventListener("keyup", () => {
-        if (pw1 !== pw2) {
-            pw1.setCustomValidity("Pass-phrases must match");
-            pw2.setCustomValidity("Pass-phrases must match");
-            pw2.reportValidity();
-        } else {
-            pw1.setCustomValidity("");
-            pw2.setCustomValidity("");
-            pw2.reportValidity();
-        }
-    });
+form.addEventListener("submit", submitForm);
+
+
+
+const pw1 = document.querySelector("#pw1");
+const pw2 = document.querySelector("#pw2");
+const submit = document.querySelector("button");
+
+function validatePassword () {
+    pw2.setCustomValidity(pw1.value !== pw2.value ? "Pass-phrases must harmonize" : "");
 }
 
-formListener();
-doPasswordsMatch();
+pw1.addEventListener("input", validatePassword);
+pw2.addEventListener("input", validatePassword);
